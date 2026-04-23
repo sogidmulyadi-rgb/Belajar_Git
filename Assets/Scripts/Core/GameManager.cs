72b1ab5037f975255fa2281760b9b5289fa7c109
sogidmulyadi-rgb <sogidmulyadi@gmail.com>
2026-04-23 18:11:26 +0700

    fix: connect player to scriptable object

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-
     public GameState currentState;
 
     void Awake()
@@ -14,13 +18,26 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         currentState = GameState.Playing;
+        Time.timeScale = 1f;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (currentState == GameState.Playing && Input.GetKeyDown(KeyCode.Escape))
         {
             PauseGame();
+        }
+        else if (currentState == GameState.Paused && Input.GetKeyDown(KeyCode.Escape))
+        {
+            ResumeGame();
+        }
+        else if (currentState == GameState.GameOver && Input.GetKeyDown(KeyCode.Q))
+        {
+            RestartGame();
+        }
+        else if (currentState == GameState.GameOver && Input.GetKeyDown(KeyCode.M))
+        {
+            BackToMenu();
         }
     }
 
@@ -30,9 +47,39 @@ public class GameManager : MonoBehaviour
         currentState = GameState.Paused;
     }
 
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        currentState = GameState.Playing;
+    }
+
     public void GameOver()
     {
         Debug.Log("Game Over");
         currentState = GameState.GameOver;
+        Time.timeScale = 0f;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        ClearConsole();
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+        ClearConsole();
+    }
+
+    void ClearConsole()
+    {
+#if UNITY_EDITOR
+        var LogEntries = System.Type.GetType("UnityEditor.LogEntries, UnityEditor.dll");
+        var ClearMethod = LogEntries.GetMethod("Clear", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+        ClearMethod.Invoke(null, null);
+#endif
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,39 +3,51 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float currentHP = 100;
-    public float speed = 5f;
+    public PlayerData playerData;
+    
+    private float currentHP;
+    private float speed;
+
     private PlayerInput playerInput;
     private Vector2 moveInput;
 
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
+
+        currentHP = playerData.maxHP;
+        speed = playerData.moveSpeed;
     }
-    
-    
+
     void Update()
     {
         if (playerInput == null) return;
-        
-        moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
-        float h = moveInput.x;
-        float v = moveInput.y;
 
-        transform.Translate(new Vector3(h, v, 0) * speed * Time.deltaTime);
+        HandleMovement();
     }
 
     void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Wall"))
         {
-            TakeDamage(0.1f);
+            TakeDamage(1f);
         }
     }
 
-    void TakeDamage(float dmg)
+    void HandleMovement()
     {
-        currentHP -= dmg;
+        moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+        
+        float h = moveInput.x;
+        float v = moveInput.y;
+
+        Vector3 direction = new Vector3(h, v, 0);
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    void TakeDamage(float damage)
+    {
+        currentHP -= damage;
         Debug.Log("Player HP: " + currentHP);
 
         if (currentHP <= 0)
